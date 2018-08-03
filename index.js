61ffb280e6b77e483a838aac4984464ce2ebe65c
Spencer Stiglets <s.stiglets@gmail.com>
2018-08-03 05:12:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function shout(string){
   return string.toUpperCase()
 }
+
 function whisper(string){
   return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(${string})
 }
 
 function logWhisper(string){
