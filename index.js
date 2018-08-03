b78caca6f5c4a47c732dc90b36309d1441594b50
Spencer Stiglets <s.stiglets@gmail.com>
2018-08-03 05:24:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){
 
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
+  if (string.toLowerCase === string) {
     return "I can\'t hear you!"
   } else if (string === string.toUpperCase) {
     return "YES INDEED!"
